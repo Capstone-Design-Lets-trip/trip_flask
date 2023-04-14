fa6b8d116e8a7dcd945ba321a035da7466541169
geoniklee <110885375+geoniklee@users.noreply.github.com>
2023-04-14 22:11:47 +0900

    임시모델 테스트

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import requests
+from att_recommend import *
 from flask import Flask, jsonify, request, render_template, redirect
-
 app = Flask(__name__)
+
 
 @app.route("/check")
 def check():
@@ -11,25 +12,19 @@ def check():
 def hello_world():
     return 'Hello, World!', 200
 
-@app.route('/echo_call/<id>/<password>') #get echo api
-def get_echo_call(id, password):
-    return jsonify(id), jsonify(password), 200
-    # return jsonify({"param": param}), 200
-
 @app.route('/echo_call', methods=['POST']) #post echo api
 def post_echo_call():
     param = request.get_json()
     print("[GET] done")
+    print(param.get('id'))
+    print(param.get('password'))
+    print(param.get('mood'))
+
     return jsonify(param), 200
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/test_model',methods=['POST'])
+def test_model():
+    return json.loads(att_recommend('한큐백화점')),200
 
-# @app.route('/echo_call', methods=['POST'])
-# def post_echo_call():
-#     data = request.get_json()
-#     id = data.get('id')
-#     mood = data.get('mood')
-#     password = data.get('password')
-#     # do something with id, mood, and password
-#     return jsonify(data), 200
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
