24198a2a13c13718ac85e929b1099490b71ebbcd
geoniklee <110885375+geoniklee@users.noreply.github.com>
2023-05-25 02:35:46 +0900

    3214

--- att_list_by_ts.py
+++ att_list_by_ts.py
@@ -14,6 +14,7 @@ def make_att_list_by_TS(TS_list='', path='', user_df_path='',city='',name=''):
   print(name)
   tmp =pd.read_csv('./clustering_'+city+'/'+name+'_sorted'+'.csv')
   sorted_total_clustering_box = list(tmp)
+  print(sorted_total_clustering_box)
   user_df = pd.read_csv(user_df_path)
   score_box = {}
   score = 15
