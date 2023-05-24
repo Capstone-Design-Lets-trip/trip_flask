a6bbc955ebaf68ef405b6ce2c9b8ed5882260359
geoniklee <110885375+geoniklee@users.noreply.github.com>
2023-05-25 01:15:03 +0900

    32141243

--- app.py
+++ app.py
@@ -121,7 +121,7 @@ def generate_again():
         end_time=datetime.datetime.strptime(param.json().get('endDate').replace('T',' '),format)
         name=param.json().get('email')
         TS_list = Thompson_Sampling(name, '', reco=1, total_Osakak_df="./total_Osaka.csv")
-        result_2 = make_att_list_by_TS(TS_list, path="./total_Osaka.csv", user_df_path="./member_info/"+name+".csv", param.json().get('city'),param.json().get('email'))
+        result_2 = make_att_list_by_TS(TS_list, path="./total_Osaka.csv", user_df_path="./member_info/"+name+".csv", city=param.json().get('city'), name=param.json().get('email'))
         result_3 = attraction_route_recommend(result_2, start_time, end_time, './Osaka_time.csv','./User_df.csv','./total_Osaka.csv',param.json().get('travel_start'),param.json().get('travel_end'))
         response = requests.get("http://letstrip.shop:8080/tour/course", json=result_3)
         return response.text
@@ -130,7 +130,7 @@ def generate_again():
         end_time = datetime.datetime.strptime(param.json().get('endDate').replace('T', ' '), format)
         name = param.json().get('email')
         TS_list = Thompson_Sampling(name, '', reco=1, total_Osakak_df="./total_Dokyo.csv")
-        result_2 = make_att_list_by_TS(TS_list, path="./total_Dokyo.csv.csv",user_df_path="./member_info_dokyo/"+name+".csv", param.json().get('city'), param.json().get('email'))
+        result_2 = make_att_list_by_TS(TS_list, path="./total_Dokyo.csv.csv",user_df_path="./member_info_dokyo/"+name+".csv", city=param.json().get('city'), name=param.json().get('email'))
         result_3 = attraction_route_recommend(result_2, start_time, end_time, './Tokyo_time.csv', './User_df.csv',
                                               './total_Dokyo.csv', param.json().get('travel_start'), param.json().get('travel_end'))
         response = requests.get("http://letstrip.shop:8080/tour/course", json=result_3)
